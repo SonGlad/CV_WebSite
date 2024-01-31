169bc82108812f11307ad6a6bfc0dd8dd8f58932
SonGlad <okoshevy@gmail.com>
2024-01-31 22:55:01 +0200

    try

--- src/components/Skills/ReactIcons/Icons.jsx
+++ src/components/Skills/ReactIcons/Icons.jsx
@@ -1,69 +1,70 @@
 import { NavLink } from "react-router-dom";
 import {ReactComponent as LinkIcon} from "../../../images/svg-icons/link.svg";
 import { IconsStyledList } from "./Icons.styled";
-import IconReact from "./IconsList/ReactIcon";
-import IconRedux from "./IconsList/ReduxIcon";
-import IconHTML from "./IconsList/HtmlIcon";
-import IconJavaScript from "./IconsList/JavaScriptIcon";
-import IconCSS from "./IconsList/CssIcon";
-import IconGit from "./IconsList/GitIcon";
+// import IconReact from "./IconsList/ReactIcon";
+// import IconRedux from "./IconsList/ReduxIcon";
+// import IconHTML from "./IconsList/HtmlIcon";
+// import IconJavaScript from "./IconsList/JavaScriptIcon";
+// import IconCSS from "./IconsList/CssIcon";
+// import IconGit from "./IconsList/GitIcon";
 import IconGitHub from "./IconsList/GitHubIcon";
-import IconVS from "./IconsList/VisualStudioIcon";
-import IconNode from "./IconsList/NodeJsIcon";
-import IconSASS from "./IconsList/SassIcon";
-import IconHandlebars from "./IconsList/HandlebarsIcon";
-import IconMongoDB from "./IconsList/MongoDbIcon";
-import IconAxios from "./IconsList/AxiosIcon";
-import IconPostman from "./IconsList/PostmanIcon";
-import IconReactNative from "./IconsList/ReactNativeIcon";
-import IconTypeScript from "./IconsList/TypeScriptIcon";
-import IconVue from "./IconsList/VueIcon";
-import IconNext from "./IconsList/NextjsIcon";
+// import IconVS from "./IconsList/VisualStudioIcon";
+// import IconNode from "./IconsList/NodeJsIcon";
+// import IconSASS from "./IconsList/SassIcon";
+// import IconHandlebars from "./IconsList/HandlebarsIcon";
+// import IconMongoDB from "./IconsList/MongoDbIcon";
+// import IconAxios from "./IconsList/AxiosIcon";
+// import IconPostman from "./IconsList/PostmanIcon";
+// import IconReactNative from "./IconsList/ReactNativeIcon";
+// import IconTypeScript from "./IconsList/TypeScriptIcon";
+// import IconVue from "./IconsList/VueIcon";
+// import IconNext from "./IconsList/NextjsIcon";
 import Profile from "../../../utils/profile.json";
 
 
-const IconComponents = [
-    IconHTML, 
-    IconCSS,
-    IconJavaScript,
-    IconReact, 
-    IconRedux, 
-    IconVS,
-    IconGitHub,
-    IconGit,
-    IconSASS,
-    IconHandlebars,
-    IconAxios,
-    IconNode,
-    IconMongoDB,
-    IconPostman,
-    IconReactNative,
-    IconTypeScript,
-    IconNext,
-    IconVue,
-];
+// const IconComponents = [
+//     IconHTML, 
+//     IconCSS,
+//     IconJavaScript,
+//     IconReact, 
+//     IconRedux, 
+//     IconVS,
+//     IconGitHub,
+//     IconGit,
+//     IconSASS,
+//     IconHandlebars,
+//     IconAxios,
+//     IconNode,
+//     IconMongoDB,
+//     IconPostman,
+//     IconReactNative,
+//     IconTypeScript,
+//     IconNext,
+//     IconVue,
+// ];
 
 
 export const Icons = () => {
     const {skills} = Profile;
+    const {React, link} = skills;
 
-    const newIconComponents = IconComponents.map((IconComponent) => {
-        const id = IconComponent.name.replace("Icon", "");
-        const { [id]: value, link } = skills.find(skill => skill.hasOwnProperty(id)) || {};
+    // const newIconComponents = IconComponents.map((IconComponent) => {
+    //     const id = IconComponent.name.replace("Icon", "");
+    //     const { [id]: value, link } = skills.find(skill => skill.hasOwnProperty(id)) || {};
         
-        return {
-            id,
-            Component: IconComponent,
-            value,
-            link,
-        };
-    }).filter(({ value, link }) => value !== undefined && link !== undefined);
+    //     return {
+    //         id,
+    //         Component: IconComponent,
+    //         value,
+    //         link,
+    //     };
+    // }).filter(({ value, link }) => value !== undefined && link !== undefined);
 
   
     
     return(
         <IconsStyledList>
-            {newIconComponents.map(({id, Component, value, link}) => (
+            {/* {newIconComponents.map(({id, Component, value, link}) => (
                 <li key={id} className="icons-list">
                     <Component className='icons'/>
                     <div className="value-cont">
@@ -76,7 +77,19 @@ export const Icons = () => {
                         </NavLink>
                     </div>
                 </li>
-            ))}
+            ))} */}
+            <li className="icons-list">
+                <IconGitHub className='icons'/>
+                <div className="value-cont">
+                        <p className="value-text">{React}</p>
+                        <NavLink className='redirect-link' to={link}
+                            aria-label="redirect link"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            <LinkIcon className="redirect-icon" width={24} height={24}/>
+                        </NavLink>
+                    </div>
+            </li>
         </IconsStyledList>
     )
 };
